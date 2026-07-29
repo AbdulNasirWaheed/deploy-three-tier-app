35901547b617b596a9d28414fa9ac35f415acccc
AbdulNasirWaheed <AbdulNasirWaheed@users.noreply.github.com>
2026-07-30 04:19:49 +0500

    Update API base URL in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 function App() {
   const [items, setItems] = useState([]);
   const [name, setName] = useState('');
-  const API_BASE_URL = 'http://107.23.51.201:5003';
+  const API_BASE_URL = 'http://3.87.239.2:5003';
 
   const fetchItems = async () => {
     const res = await axios.get(`${API_BASE_URL}/api/items`);
